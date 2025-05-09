2c8adfc13fffeee10ef772076da70870c4f15f5f
KlintBzl <klintscouts@gmail.com>
2025-05-09 18:04:42 -0300

    ta bonito

--- Atv2/js/script.js
+++ Atv2/js/script.js
@@ -1,11 +1,11 @@
 const frm = document.querySelector("form") 
 const hagadois = document.querySelector("h2")
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let maiores = [0];
 frm.addEventListener("submit", (e) => {
     
-    let maiores = numeros.filter(n => n % frm.valor.value);
+    maiores = numeros.filter(n => n % frm.valor.value);
     e.preventDefault();
+    hagadois.innerText =`Sequência: ${maiores}`;
 });
 
-
-hagadois.innerText(` Sequência: ${maiores}`);
