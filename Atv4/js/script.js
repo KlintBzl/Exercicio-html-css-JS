12853d3cd26d6fd827e88424c2d8514e045fdfca
KlintBzl <klintscouts@gmail.com>
2025-05-12 17:10:32 -0300

    atualização

--- Atv4/js/script.js
+++ Atv4/js/script.js
@@ -1,11 +1,9 @@
 const frm = document.querySelector("form");
 const resposta = document.querySelector("h2");
 
+
 frm.addEventListener("submit", (e) => {
 
     e.preventDefault();
 
-    if(frm.adicao){
-        console.log("blablabla")
-    }
 })
