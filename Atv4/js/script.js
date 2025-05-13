982f7fea8488b19596ca68e35e9b93b16f0295aa
KlintBzl <klintburzlaff@gmail.com>
2025-05-13 20:56:33 -0300

    Atualizacao

--- Atv4/js/script.js
+++ Atv4/js/script.js
@@ -1,9 +1,29 @@
 const frm = document.querySelector("form");
 const resposta = document.querySelector("h2");
 
+function verificarGenero() {
+  const radios = document.getElementsByName("decidir");
+  let valorSelecionado = null;
+
+  for (const radio of radios) {
+    if (radio.checked) {
+      valorSelecionado = radio.value;
+      break;
+    }
+  }
+
+  if (valorSelecionado) {
+    alert("Gênero selecionado: " + valorSelecionado);
+  } else {
+    alert("Nenhuma opção selecionada.");
+  }
+}
 
 frm.addEventListener("submit", (e) => {
+e.preventDefault();
 
-    e.preventDefault();
+verificarGenero();
+
+    
 
 })
